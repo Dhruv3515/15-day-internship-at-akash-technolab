e8bc60c5f4367d2ec510bcaf80d3eed934878d6d
Dhruv3515 <84258899+Dhruv3515@users.noreply.github.com>
2021-05-26 14:02:04 +0530

    Add files via upload

---
+++ day2/grade.php
@@ -0,0 +1,24 @@
+<?php
+echo "<h4>Day 2 intern at akash technolab</h4>";
+$marks = 91;
+if($marks>33)
+{
+	if($marks>90)
+	{
+		echo "Grade A";
+	}
+	elseif($marks>70)
+	{
+		echo "Grade B";
+	}
+	elseif($marks>50)
+	{
+		echo "Grade C";
+	}
+  
+}
+else
+{
+echo "you fail in exam";
+}
+?>
---
+++ day2/switchcase.php
@@ -0,0 +1,38 @@
+<?php
+echo "<h4>Day 2 intern at akash technolab</h4>";
+$day=4;
+switch($day){
+	case 1:
+	echo"monday";
+	break;
+	
+	case 2:
+	echo"tuesday";
+	break;
+	
+	case 3:
+	echo"wednesday";
+	break;
+	
+	case 4:
+	echo"thursday";
+	break;
+	
+	case 5:
+	echo"friday";
+	break;
+	
+	case 6:
+	echo"saturday";
+	break;
+	
+	case 7:
+	echo"sunday";
+	break;
+	
+	default:
+	echo"please enter between 1 to 7";
+	break;
+}
+
+?>
