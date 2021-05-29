f36870049213eb2d9b6d4591ca8315e839ad2cc8
Dhruv3515 <84258899+Dhruv3515@users.noreply.github.com>
2021-05-29 18:22:15 +0530

    Add files via upload

---
+++ day 4 project 1/aboutus.php
@@ -0,0 +1,30 @@
+  <div class="best-features">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-heading">
+              <h2>About Food Cart</h2>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="left-content">
+              <h4>Looking for the best Food?</h4>
+              <p><a rel="nofollow"  <a rel="nofollow" href="https://templatemo.com/contact"></p>
+              <ul class="featured-list">
+                <li><a href="#">Simple</a></li>
+                <li><a href="#">gujrati</a></li>
+                <li><a href="#">panjabi</a></li>
+                <li><a href="#">south-indian</a></li>
+                <li><a href="#">Italian</a></li>
+              </ul>
+              <a href="about.html" class="filled-button">Read More</a>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="right-image">
+              <img src="assets/images/dinner.jpg" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
---
+++ day 4 project 1/contact.php
@@ -0,0 +1,53 @@
+    <header class="">
+      <nav class="navbar navbar-expand-lg">
+        <div class="container">
+          <a class="navbar-brand" href="index.html"><h2>Food <em>Cart</em></h2></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="index.html">Home
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li> 
+              <li class="nav-item">
+                <a class="nav-link" href="product1.php">Our Products</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.php">About Us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+
+    <!-- Page Content -->
+    <!-- Banner Starts Here -->
+    <div class="banner header-text">
+      <div class="owl-banner owl-carousel">
+        <div class="banner-item-01">
+          <div class="text-content">
+            <h4>Best Offer</h4>
+            <h2>On friday</h2>
+          </div>
+        </div>
+        <div class="banner-item-02">
+          <div class="text-content">
+            <h4>Flash Deals</h4>
+            <h2>Get your Food At home</h2>
+          </div>
+        </div>
+        <div class="banner-item-03">
+          <div class="text-content">
+            <h4>Last Minute</h4>
+            <h2>Grab last minute deals</h2>
+          </div>
+        </div>
+      </div>
+    </div>
