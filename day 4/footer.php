6464f31d82327379d9197e753e042f4abe2534af
Dhruv3515 <84258899+Dhruv3515@users.noreply.github.com>
2021-05-30 14:54:33 +0530

    Add files via upload

---
+++ day 4/footer.php
@@ -0,0 +1,7 @@
+ <footer class="row">
+                <p class="mb-0 w-100 text-center col-12">
+                     &copy; Devloped By Dhruv R Bavadiya 
+                    
+						<a rel="nofollow" href="https://templatemo.com" class="tm-text-link"></a>
+                </p>
+            </footer>
